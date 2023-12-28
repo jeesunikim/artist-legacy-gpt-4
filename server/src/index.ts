754556bb542c63a6d32b30af2b92ad48f537474e
Jeesun Kim <jeesunkim@Jeesuns-MacBook-Pro.local>
2023-12-28 16:04:17 +0900

    remove comments

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,7 @@
-import http from "http";
 import path from "path";
 import express from "express";
-import favicon from "serve-favicon";
 import logger from "morgan";
-import methodOverride from "method-override";
-import session from "express-session";
 import bodyParser from "body-parser";
-import multer from "multer";
-import errorHandler from "errorhandler";
 
 import "dotenv/config";
 
@@ -21,17 +15,9 @@ app.use("/public", express.static(path.join(__dirname, "/../../public")));
 
 app.use(logger("log"));
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.sendFile(path.join(__dirname, "/../../client/index.html"));
 });
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'views', 'index.html'));
-// });
-
-// app.use("/chat", chatRouter);
-// app.use("/images", imagesRouter);
-// app.use("/files", fileRouter);
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
